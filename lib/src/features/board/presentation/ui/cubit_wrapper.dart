61c463a286dd08dddd5feb4492d3aa67a8f04c50
Nikonovich Nikita <nikonovichnd@gmail.com>
2024-06-12 15:05:26 +0300

    add: board

--- lib/src/features/board/presentation/ui/cubit_wrapper.dart
+++ lib/src/features/board/presentation/ui/cubit_wrapper.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import "dart:convert";
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'board.dart';
 import '../../api.dart';
 import '../../data/data.dart';
 import '../cubit/tasks_cubit.dart';
@@ -34,26 +34,9 @@ class _DisplayStatusData extends StatelessWidget {
       builder: (context, state) => switch (state) {
         TasksInitial() => const Text('Initial'),
         TasksLoading() => const CircularProgressIndicator(),
-        TasksLoaded() => const _TasksDisplayWidget(),
+        TasksLoaded() => const Board(),
         TasksError() => const Text('Error'),
       },
-    );
-  }
-}
-
-class _TasksDisplayWidget extends StatelessWidget {
-  const _TasksDisplayWidget();
-
-  @override
-  Widget build(BuildContext context) {
-    final state = context.read<TasksCubit>().state as TasksLoaded;
-    return SingleChildScrollView(
-      child: Column(
-        children: state.data.map((e) {
-          final encodedLatin1 = const Latin1Codec().encode(e.name);
-          return Text(utf8.decode(encodedLatin1));
-        }).toList(),
-      ),
     );
   }
 }
